8184a8ad39fd99aceda1c985dcfb57f8f19f68b1
github username <github email address>
2019-11-22 14:25:13 -0800

    Gameboard container

---
+++ shuffled-cards-frontend/src/containers/gameboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Gameboard extends Component {
+
+    render(){
+        return(
+            <Div>Gameboard</Div>
+        )
+    }
+}
+
+export default Gameboard
