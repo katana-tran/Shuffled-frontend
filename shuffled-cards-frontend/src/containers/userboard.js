e07474e3b50ff8eb6f725edd1bd0ac5ed8a6c5c5
github username <github email address>
2019-11-22 15:28:38 -0800

    userboard container

---
+++ shuffled-cards-frontend/src/containers/userboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Userboard extends Component {
+
+    render(){
+        return(
+            <div>Userboard</div>
+        )
+    }
+}
+
+export default Userboard;
