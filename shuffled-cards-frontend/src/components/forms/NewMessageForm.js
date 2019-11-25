23e1bde115a30b9f37fad3f6d28cf9aad65961fd
github username <github email address>
2019-11-24 21:48:02 -0800

    starting chat feature

---
+++ shuffled-cards-frontend/src/components/forms/NewMessageForm.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { API_ROOT, HEADERS } from '../../constants/API.constants'
+
+class NewMessageForm extends React.Component {
+    state = {
+        text: '',
+        chat_id: this.props.chat_id
+    };
+
+    componentWillReceiveProps = nextProps => {
+        this.setState({ chat_id: nextProps.chat_id });
+    };
+
+    handleChange = e => {
+        this.setState({ text: e.target.value });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        fetch(`${API_ROOT}/messages`, {
+            method: 'POST',
+            headers: HEADERS,
+            body: JSON.stringify(this.state)
+        });
+        this.setState({ text: '' });
+    };
+
+    render = () => {
+    return (
+        <div className="newMessageForm">
+            <form onSubmit={this.handleSubmit}>
+                <label>New Message:</label>
+                <input
+                type="text"
+                value={this.state.text}
+                onChange={this.handleChange}
+                />
+                <input type="submit" />
+            </form>
+        </div>
+    );
+    }
+}
+
+export default NewMessageForm
